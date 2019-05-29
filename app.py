ec1d09fb303e919df829caa66e6b24e5f16eccfe
Sean Gray <sean.myles.gray@gmail.com>
2019-05-29 10:36:59 +0100

    nothings working still

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from pony.orm import Database
-
+from config.environment import db_uri
 app = Flask(__name__)
 db = Database()
+
+db.bind('postgres', db_uri)
+
+db.generate_mapping(create_tables=True)
 
 @app.route('/')
 def home():
--- models/Beer.py
+++ models/Beer.py
@@ -1,11 +1,14 @@
 from app import db
 from pony.orm import Required, Optional
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_load
+
+from .Brewery import Brewery
+from .Style import Style
 
 class Beer(db.Entity):
     name = Required(str)
-    brewery = Required(str)
-    style = Required(str)
+    brewery = Required('Brewery')
+    style = Required('Style')
     hops = Optional(str)
     region = Required(str)
     abv = Required(float)
@@ -16,11 +19,25 @@ class Beer(db.Entity):
 class BeerSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
-    brewery = fields.Str(required=True)
-    style = fields.Str(required=True)
+    brewery = fields.Nested('BrewerySchema', exclude=('beers',), dump_only=True)
+    style = fields.Nested('StyleSchema', exclude=('beers',), dump_only=True)
     hops = fields.Str()
     region = fields.Str(required=True)
     abv = fields.Float(required=True)
     price = fields.Float(required=True)
     tasting_notes = fields.Str(required=True)
     user = fields.Nested('UserSchema', exclude=('email', 'sandwiches'))
+
+    @post_load
+    def load_brewery(self, data):
+        data['brewery'] = Brewery.get(id=data['brewery_id'])
+        del data['brewery_ids']
+
+        return data
+
+    @post_load
+    def load_style(self, data):
+        data['style'] = Style.get(id=data['style_id'])
+        del data['style_id']
+
+        return data
